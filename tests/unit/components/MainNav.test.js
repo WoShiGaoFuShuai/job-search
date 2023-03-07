8f99fb75c25a99ccbe2a45a19136aced5f5295f4
WoShiGaoFuShuai <efremovmaxim95@mail.ru>
2023-03-07 05:26:11 +0300

    added test for v-for items

--- tests/unit/components/MainNav.test.js
+++ tests/unit/components/MainNav.test.js
@@ -8,4 +8,14 @@ describe("MainNav", () => {
     const companyName = screen.getByText("Mali Careers")
     expect(companyName).toBeInTheDocument()
   })
+
+  it("renders menu items in the navbar", () => {
+    const arrayInMainNav = ["Teams", "Location", "Life at the company", "How we fire", "Students"]
+
+    render(MainNav)
+    const navMenuItems = screen.getAllByRole("listitem")
+    const navMenuTexts = navMenuItems.map((item) => item.textContent)
+
+    expect(navMenuTexts).toEqual(arrayInMainNav)
+  })
 })
