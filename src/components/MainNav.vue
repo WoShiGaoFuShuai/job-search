9bd397f774448e1d82e37f0ae507efcc27d72606
WoShiGaoFuShuai <efremovmaxim95@mail.ru>
2023-03-07 05:12:32 +0300

    nav bar is finished with v-for

---
+++ src/components/MainNav.vue
@@ -0,0 +1,43 @@
+<template>
+  <header class="w-full text-sm">
+    <div class="fixed top-0 left-0 h-16 w-full bg-white">
+      <div class="border-brand-gray-1 mx-auto flex h-full flex-nowrap border-b px-8">
+        <a
+          href="/"
+          class="flex h-full items-center text-xl"
+        >
+          {{ company }}</a
+        >
+
+        <nav class="ml-12">
+          <ul class="flex h-full list-none items-center space-x-8">
+            <li
+              v-for="item in menuItems"
+              :key="item"
+              class="h-full"
+            >
+              <a
+                href="#"
+                class="flex h-full items-center py-2.5"
+              >
+                {{ item }}
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "MainNav",
+  data() {
+    return {
+      company: "Mali Careers",
+      menuItems: ["Teams", "Location", "Life at the company", "How we fire", "Students"]
+    }
+  }
+}
+</script>
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme")
+
+module.exports = {
+  content: ["./index.html", "./src/**/*.{vue,js,ts}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Open Sans", ...defaultTheme.fontFamily.sans],
+        colors: { "brand-gray-1": "#dadceo", "brand-blue-1": "#1967d2", "brand-green-1": "#137333" }
+      }
+    }
+  },
+  plugins: []
+}
