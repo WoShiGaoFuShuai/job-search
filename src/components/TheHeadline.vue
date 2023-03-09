c12bf82864ab1a3b27664e667f58d78c4c568f3d
WoShiGaoFuShuai <efremovmaxim95@mail.ru>
2023-03-09 07:03:48 +0300

    Added logic to THEHeadline and computed css class
    Every 3s text in span will change with different colors

--- src/components/TheHeadline.vue
+++ src/components/TheHeadline.vue
@@ -1,9 +1,63 @@
 <template>
-  <section>Hello! I'm the headline</section>
+  <section>
+    <h1>
+      <span :class="actionClasses">{{ action }}</span> <br />
+      for everyone!
+    </h1>
+    <h2>Find your next job at Mali Corp.</h2>
+  </section>
 </template>
 
 <script>
 export default {
-  name: "TheHeadline"
+  name: "TheHeadline",
+  data() {
+    return {
+      action: "Build",
+      interval: null
+    }
+  },
+  computed: {
+    actionClasses() {
+      return {
+        [this.action.toLocaleLowerCase()]: true
+      }
+    }
+  },
+  created() {
+    this.changeAction()
+    console.log(this.interval)
+  },
+  beforeUnmount() {
+    clearInterval(this.interval)
+  },
+
+  methods: {
+    changeAction() {
+      const actions = ["Build", "Create", "Design", "Code"]
+      this.interval = setInterval(() => {
+        const currentIndex = actions.indexOf(this.action) //0
+        const nextIndex = (currentIndex + 1) % 4 // 1
+        this.action = actions[nextIndex]
+      }, 3000)
+    }
+  }
 }
 </script>
+<style scoped>
+.build {
+  color: #1a73e8;
+}
+
+.create {
+  color: #34a853;
+}
+
+.design {
+  color: #f9ab00;
+}
+
+.code {
+  color: #d93025;
+}
+</style>
