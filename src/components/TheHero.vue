e458f5def0cc46b9d87d6a9670235d4ffcf157d4
WoShiGaoFuShuai <efremovmaxim95@mail.ru>
2023-03-09 07:11:00 +0300

    fixed styles in TheHero and TheHeadline

--- src/components/TheHero.vue
+++ src/components/TheHero.vue
@@ -1,6 +1,6 @@
 <template>
   <main>
-    <section class="flex h-screen flex-col bg-yellow-200 pt-10 pb-20 text-white">
+    <section class="flex h-screen flex-col bg-yellow-200 pt-10 pb-20">
       <div class="grid grid-cols-12">
         <div class="col-span-1 col-start-1">1 column</div>
         <div class="col-span-5 col-start-2">
